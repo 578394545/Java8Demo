136862c1cb8ebdf296fd97ff0fb54ba7b946d75e
creatorL <578394545@qq.com>
2016-11-30 15:11:33 +0800

    添加注释

--- src/java8demo/lxy/optional/Demo1.java
+++ src/java8demo/lxy/optional/Demo1.java
@@ -7,18 +7,18 @@ import java.util.Optional;
 public class Demo1 {
 	
 	public static void main(String[] args) {
-		Optional< String > fullNameNull = Optional.ofNullable( null );
-		System.out.println( "Full Name is set? " + fullNameNull.isPresent() );       
-		System.out.println( "Full Name: " + fullNameNull.orElseGet( () -> "[none]" ) );
-		System.out.println( fullNameNull.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+		Optional< String > fullNameNull = Optional.ofNullable( null );//为指定的值创建一个Optional，如果指定的值为null，则返回一个空的Optional。
+		System.out.println( "是否有值 ? " + fullNameNull.isPresent() );//判断是否有值，如果值存在返回true，否则返回false。
+		System.out.println( "值 : " + fullNameNull.orElseGet( () -> "[none]" ) );//接受Supplier接口的实现，生成默认值
+		System.out.println( fullNameNull.map( s -> "你好  " + s + "!" ).orElse( "你好  else!" ) );//如果有值则将其返回，否则返回指定的其它值。
 		System.out.println(fullNameNull);
 		
 		System.out.println();
 		
 		Optional< String > fullName = Optional.ofNullable( "Soar" );
-		System.out.println( "Full Name is set? " + fullName.isPresent() );       
-		System.out.println( "Full Name: " + fullName.orElseGet( () -> "[none]" ) );
-		System.out.println( fullName.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+		System.out.println( "是否有值 ? " + fullName.isPresent() );       
+		System.out.println( "值 : " + fullName.orElseGet( () -> "[none]" ) );
+		System.out.println( fullName.map( s -> "你好  " + s + "!" ).orElse( "你好  else!" ) );
 		System.out.println(fullName);
 		
 		System.out.println();
@@ -28,9 +28,9 @@ public class Demo1 {
 		List<String> list2 = Arrays.asList("1","2","3");
 		
 		Optional< List<String> > fullNameList = Optional.ofNullable( list );
-		System.out.println( "Full Name is set? " + fullNameList.isPresent() );       
-		System.out.println( "Full Name: " + fullNameList.orElseGet( () -> list2 ) );
-		System.out.println( fullNameList.map( s -> "Hey " + s + "!" ).orElse( "Hey Stranger!" ) );
+		System.out.println( "是否有值 ? " + fullNameList.isPresent() );       
+		System.out.println( "值 : " + fullNameList.orElseGet( () -> list2 ) );
+		System.out.println( fullNameList.map( s -> "你好  " + s + "!" ).orElse( "你好  else!" ) );
 		System.out.println(fullNameList);
 	}
 }
